a607c0597060be61298b29dcbf71dbd2e5ec7741
3h-FT <15h.nomura@gmail.com>
2025-03-26 15:57:20 +0900

    fix:PostgreSQLのデータベース名をテスト環境用に統一

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class TopControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
+    # fixturesからユーザーを取得
+    user = users(:one)
+
+    # サインイン
+    sign_in user
+
+    # GETリクエストを送信
     get top_index_url
+
+    # レスポンスが成功することを確認
     assert_response :success
   end
 end
