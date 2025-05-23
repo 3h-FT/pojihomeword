21b16d7ee6ce618ee2d1bd093e681ab0fd9ddf56
3h-FT <15h.nomura@gmail.com>
2025-05-23 20:34:01 +0900

    # This is a combination of 19 commits.
    fix:CIの時とローカルの時のseleniumのURLの修正
    
    fix:seleniumの名前を明示
    
    fix:seleniumの修正
    
    fix:seleniumの修正02
    
    add:接続先の統一
    
    add:ポート先の修正
    
    add:ciにdocker環境を追加
    
    add:ciにdockerの修正
    
    # This is the commit message #12:
    
    add:ciのポート削除
    
    # This is the commit message #17:
    
    add:シークレットキーの修正
    
    # This is the commit message #19:
    
    add：権限エラーの修正

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+unless ENV["CI"]
+  secret_path = Rails.root.join("tmp/local_secret.txt")
+  File.binwrite(secret_path, "abc123") unless File.exist?(secret_path)
+end
