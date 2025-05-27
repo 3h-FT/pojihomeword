16b731a5d1a463cde0fbe7194e6ccb916b9dbbdf
3h-FT <15h.nomura@gmail.com>
2025-05-27 16:04:15 +0900

    add:お問い合わせフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,9 @@ Rails.application.routes.draw do
     end
   end
   resources :post_favorites, only: %i[create destroy]
-
+  
+  resources :contacts, only: [:new, :create]
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_25_084013) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_053208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_25_084013) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "positive_words", force: :cascade do |t|
