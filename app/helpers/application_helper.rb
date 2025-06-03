a2ffe88835d133e1379bcf9f8ed0cf5a82572124
3h-FT <15h.nomura@gmail.com>
2025-06-03 19:03:01 +0900

    add:静的OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'ポジほめワード',
+      title: 'ポジほめワード',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ポジティブな言葉を「知る」・「ためる」・「共有する」ことのできるアプリです。',
+      keywords: 'ポジティブ,ほめる,ワード,知る,ためる,共有',
+      canonical: request.original_url,
+      separator: '|', #Webサイト名とページタイトルを区切るために使用されるテキスト
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('default_ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('default_ogp.png')
+      }
+    }
+  end
 end
