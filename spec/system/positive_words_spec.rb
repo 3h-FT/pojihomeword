42b5221440c30c1185abf016c84a8064e9367565
3h-FT <15h.nomura@gmail.com>
2025-06-06 18:54:05 +0900

    fix:テストコード修正

--- spec/system/positive_words_spec.rb
+++ spec/system/positive_words_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "PositiveWords", type: :system do
   let(:user) { create(:user) }
-  let!(:target) { Target.find_or_create_by(name: "自分自身") }
-  let!(:situation) { Situation.find_or_create_by(name: "自分の成功を祝う", target: target) }
-  let!(:positive_word) { PositiveWord.find_or_create_by(word: "よく頑張ったね、これからが楽しみだ！", target: target, situation: situation) }
+
+  # is_seeded: true を明示的に設定してデータを作成
+  let!(:target_seeded) { Target.find_or_create_by!(name: "自分自身", is_seeded: true) }
+  let!(:situation_seeded) { Situation.find_or_create_by!(name: "自分の成功を祝う", target: target_seeded, is_seeded: true) }
+  let!(:positive_word_seeded) { PositiveWord.find_or_create_by!(word: "よく頑張ったね、これからが楽しみだ！", target: target_seeded, situation: situation_seeded, is_custom: false, user: nil) }
 
   it 'シチュエーション選択後、適したポジティブワードが表示されるか' do
     visit positive_words_path
 
+    # is_seeded: true のデータが選択肢に表示されることを確認
     select '自分自身', from: 'target_id'
     click_button '表示'
 
+    # is_seeded: true のデータが選択肢に表示されることを確認
     select '自分の成功を祝う', from: 'situation_id'
     click_button '表示'
 
