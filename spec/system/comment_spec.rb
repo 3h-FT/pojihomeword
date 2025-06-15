4d86079c9ee50a4b8830f1415b644140cb89b000
3h-FT <15h.nomura@gmail.com>
2025-06-15 21:39:20 +0900

    fix:テストコードの整備

--- spec/system/comment_spec.rb
+++ spec/system/comment_spec.rb
@@ -1,95 +1,81 @@
-#require 'rails_helper'
+# spec/system/comments_spec.rb
+require 'rails_helper'
 
-#RSpec.describe 'Comments', type: :system do
-#  let(:user) { create(:user) }
-#  let(:post) { create(:post) }
-#  let!(:comment_by_me) { create(:comment, user: user, post: post) }
-#  let!(:comment_by_others) { create(:comment, post: post) }
+RSpec.describe 'Comments', type: :system do
+  let(:user)  { create(:user) }
+  let(:post)  { create(:post) }
+  let!(:own_comment)     { create(:comment, user: user,  post: post) }
+  let!(:others_comment)  { create(:comment, post: post) }
 
-#  before do
-#    login_as(user)
-#    visit posts_path
-#    first(:css, "#post-id-#{post.id} a", text: post.post_word).click
-#  end
+  before do
+    login_as(user)
+    visit posts_path
+    first(:css, "#post-id-#{post.id} a", text: post.post_word).click
+  end
 
-#  describe 'コメント一覧表示' do
-#    it '自分のコメントが表示される' do
-#      within '#table-comment' do
-#        expect(page).to have_text(comment_by_me.body), 'コメントに「コメント内容」が表示されていません'
-#        expect(page).to have_text(comment_by_me.user.username), 'コメントに「投稿者」が表示されていません'
-#      end
-#    end
-#  end
+  describe '一覧表示' do
+    it '自分のコメントとユーザー名が表示される' do
+      within '#table-comment' do
+        expect(page).to have_text(own_comment.body), '「(own_comment.body」が表示されていません'
+        expect(page).to have_text(user.username), '「user.username」が表示されていません'
+      end
+    end
+  end
 
-#  describe 'コメント作成' do
-#    context 'コメント作成が成功した場合' do
-#      it '新しいコメントを投稿できる' do
-#        fill_in 'comment_body', with: '新規コメント'
-#        click_on '投稿'
+  describe 'コメント作成 (Turbo Stream)' do
+    context '成功' do
+      it 'フォーム送信後に一覧へ即時反映される', js: true do
+        fill_in 'コメント', with: '新規コメント'
+        click_on '投稿'
 
-#        expect(page).to have_text('コメントを投稿しました'), 'フラッシュメッセージ「コメントを投稿しました」が表示されていません'
+        # Turbo Stream でフォームがリセットされる
+        within '#comment-form' do
+          expect(find('textarea').value).to eq ''
+        end
 
-#        comment = Comment.last
-#        expect(comment.body).to eq('新規コメント'), 'ページ内に「編集ワード」が表示されていません'
-#        expect(comment.user).to eq(user), 'ページ内に「編集ワード」が表示されていません'
-#        expect(comment.post).to eq(post), 'ページ内に「編集ワード」が表示されていません'
+        within '#table-comment' do
+          expect(page).to have_text('新規コメント'), '「新規コメント」が表示されていません'
+          expect(first('h3').text).to eq(user.username), '「user.username」が表示されていません'
+        end
+      end
 
-#        within "#comment-#{comment.id}" do
-#          expect(page).to have_text('新規コメント'), 'ページ内に「編集ワード」が表示されていません'
-#          expect(page).to have_text(user.username), 'ページ内に「編集ワード」が表示されていません'
-#        end
-#      end
-#    end
+      it '自分のコメントをインラインで更新できる', js: true do
+        within "#comment-#{own_comment.id}" do
+          find('.edit-comment-button').click
+        end
 
-#    context 'コメント作成の失敗' do
-#      it 'コメント本文が空では投稿できない' do
-#        expect {
-#          fill_in 'comment_body', with: ''
-#          click_on '投稿'
-#        }.not_to change { Comment.count }
-#        expect(page).to have_text('コメントを投稿できません'), 'フラッシュメッセージ「コメントを投稿できません」が表示されていません'
-#      end
-#    end
-#  end
+        expect(page).to have_selector("turbo-frame#comment-form-#{own_comment.id}", wait: 5)
 
-#  describe 'コメントの編集・削除' do
-#    let!(:editable_comment) { create(:comment, user: user, post: post) }
+        within "turbo-frame#comment-form-#{own_comment.id}" do
+          expect(page).to have_field('コメントの編集', wait: 5)
+          fill_in 'コメントの編集', with: '更新されたコメント'
+          click_on '更新'
+        end
+  
+        within "#comment-#{own_comment.id}" do
+          expect(page).to have_text('更新されたコメント'), '編集内容が反映されていません'
+        end
+      end
+    end
+  end
 
-#    context '自分のコメントの場合' do
-#      it '編集できる' do
-#        within "#comment-#{editable_comment.id}" do
-#          find('.edit-comment-button').click
-#        end
+  describe 'コメント削除 (Turbo Stream)' do
+    it '自分のコメントを削除できる', js: true do
+      within "#comment-#{own_comment.id}" do
+        accept_confirm { find('.delete-comment-button').click }
+      end
 
-#        expect(page).to have_current_path(edit_comment_path(editable_comment))
+      # Turbo Stream remove 後に DOM から要素が消えているか
+      expect(page).not_to have_selector("#comment-#{own_comment.id}"), 'コメントが削除されていません'
+    end
+  end
 
-#        fill_in 'comment_body', with: '更新されたコメント'
-#        click_on '更新'
-
-#        expect(page).to have_current_path(post_path(post))
-#        within "#comment-#{editable_comment.id}" do
-#          expect(page).to have_text('更新されたコメント'), 'ページ内に「編集ワード」が表示されていません'
-#        end
-#      end
-
-#      it '削除できる' do
-#        within "#comment-#{editable_comment.id}" do
-#          expect(page).to have_selector('.delete-comment-button')
-#          click_button class: 'delete-comment-button'
-#        end
-
-#        expect(page).to have_text('コメントを削除しました'), 'フラッシュメッセージ「コメントを削除しました」が表示されていません'
-#        expect(page).not_to have_selector("#comment-#{editable_comment.id}")
-#      end
-#    end
-
-#    context '他人のコメントの場合' do
-#      it '編集・削除ボタンが表示されない' do
-#        within "#comment-#{comment_by_others.id}" do
-#          expect(page).not_to have_selector('.edit-comment-button')
-#          expect(page).not_to have_selector('.delete-comment-button')
-#        end
-#      end
-#    end
-#  end
-#end
+  describe '他ユーザーのコメント' do
+    it '編集・削除ボタンが表示されない' do
+      within "#comment-#{others_comment.id}" do
+        expect(page).not_to have_selector('.edit-comment-button'), '編集ボタンが表示されています'
+        expect(page).not_to have_selector('.delete-comment-button'), '削除ボタンが表示されています'
+      end
+    end
+  end
+end
