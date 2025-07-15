1e41e5e5708fe39c48f70e301a7246c6409cb2f0
3h-FT <15h.nomura@gmail.com>
2025-07-15 17:29:21 +0900

    add:管理者画面の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_06_063547) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_15_060955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_06_063547) do
     t.integer "target_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "source_type", default: 1
   end
 
   create_table "post_favorites", force: :cascade do |t|
@@ -94,6 +93,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_06_063547) do
     t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -240,3 +240,12 @@ target_data[:situations].each do |situation_name|
         end
       end
     end
+
+if ENV["ADMIN_EMAIL"].present? && ENV["ADMIN_PASSWORD"].present?
+  User.find_or_create_by!(email: ENV["ADMIN_EMAIL"]) do |user|
+    user.username = "admin"
+    user.password = ENV["ADMIN_PASSWORD"]
+    user.password_confirmation = ENV["ADMIN_PASSWORD"]
+    user.admin = true
+  end
+end
